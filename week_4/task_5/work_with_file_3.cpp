8e38c7103f1fe2e4e48dec93dbcdf8b2e5e481db
Mopre4 <educationfork@gmail.com>
2020-05-18 22:01:42 +0300

    work_with_file_3

---
+++ week_4/task_5/work_with_file_3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(){
+	ifstream input("input.txt");
+	double num;
+	if (input) {
+		while (input >> num) {
+			cout << fixed <<  setprecision(3) << num << endl;
+		}
+		
+	}
+}
